241f3a0a3901ccb3de38df56606ef37341e1f3af
Kannu Mehta <kannumehta@gmail.com>
2023-11-02 15:25:11 -0700

    Added basic themes for KMM

--- androidApp/src/main/java/com/khanalytic/kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/khanalytic/kmm/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.khanalytic.kmm.android
 
+import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,86 +24,27 @@ import com.khanalytic.kmm.SwiggyApiFactory
 import com.khanalytic.integrations.models.Menu
 import kotlinx.coroutines.launch
 
-@Composable
-fun MyApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        darkColors(
-            primary = Color(0xFFBB86FC),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    } else {
-        lightColors(
-            primary = Color(0xFF6200EE),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
-    }
-    val typography = Typography(
-        body1 = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
-    )
-    val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
-    )
-
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting(Greeting().greeting())
-                }
-            }
+            App()
         }
 
-        val cookie = "WZRK_G=00eb916684f14c91bc6bee4f4707e2fa; WZRK_G=00eb916684f14c91bc6bee4f4707e2fa; WZRK_L=%257B%257D; Swiggy_Session-alpha=30e166d0-f7ff-492b-8a95-f863d64823d9; Swiggy_Session-newSys=1; Swiggy_Session-user=6360188416; Swiggy_user_role=2; Swiggy_change_password=false; isRidBasedLogin=false; is_dot_in=1; WZRK_S_8RK-K65-846Z=%7B%22p%22%3A3%2C%22s%22%3A1697953841%2C%22t%22%3A1697953941%7D; Swiggy_Session-perms=%5B1%2C2%2C4%2C5%2C6%5D"
-        lifecycleScope.launch {
-            val swiggyApi = SwiggyApiFactory().create(cookie)
-            val menu: Menu = swiggyApi.getMenu("42076")
+//        val cookie = "WZRK_G=00eb916684f14c91bc6bee4f4707e2fa; WZRK_G=00eb916684f14c91bc6bee4f4707e2fa; WZRK_L=%257B%257D; Swiggy_Session-alpha=30e166d0-f7ff-492b-8a95-f863d64823d9; Swiggy_Session-newSys=1; Swiggy_Session-user=6360188416; Swiggy_user_role=2; Swiggy_change_password=false; isRidBasedLogin=false; is_dot_in=1; WZRK_S_8RK-K65-846Z=%7B%22p%22%3A3%2C%22s%22%3A1697953841%2C%22t%22%3A1697953941%7D; Swiggy_Session-perms=%5B1%2C2%2C4%2C5%2C6%5D"
+//        lifecycleScope.launch {
+//            val swiggyApi = SwiggyApiFactory().create(cookie)
+//            val menu: Menu = swiggyApi.getMenu("42076")
 //             logger.atInfo().log("fetched revenue: %s", swiggyApi.getOrders("42076", "2023-10-17", "2023-10-18", menu))
-            // logger.atInfo().log("fetched revenue: %s", swiggyApi.getSalesSummary("42076", "2023-10-17", "2023-10-18"))
+        // logger.atInfo().log("fetched revenue: %s", swiggyApi.getSalesSummary("42076", "2023-10-17", "2023-10-18"))
 //             logger.atInfo().log("fetched revenue: %s", swiggyApi.getComplaints("42076", "2023-10-01", "2023-10-18"))
-             logger.atInfo().log("fetched revenue: %s", swiggyApi.sendEmailReport("42076", "2023-07-27", "2023-10-26", "mehtakan@gmail.com"))
-            // logger.atInfo().log("fetched revenue: %s", swiggyApi.getBrands())
+//             logger.atInfo().log("fetched revenue: %s", swiggyApi.sendEmailReport("42076", "2023-07-27", "2023-10-26", "mehtakan@gmail.com"))
+        // logger.atInfo().log("fetched revenue: %s", swiggyApi.getBrands())
 //            logger.atInfo().log("fetched revenue: %s", swiggyApi.getMenu("42076"))
-        }
+//        }
     }
 
     companion object {
         private val logger = FluentLogger.forEnclosingClass()
-    }
-}
-
-@Composable
-fun Greeting(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Hello, Android!")
     }
 }
--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,12 +1,13 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.multiplatform.webview.cookie.Cookie
 import com.multiplatform.webview.cookie.WebViewCookieManager
 import com.multiplatform.webview.web.LoadingState
@@ -32,35 +37,40 @@ import org.jetbrains.compose.resources.painterResource
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    Napier.base(DebugAntilog())
-    val url = "https://partner.swiggy.com/login"
-    MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        val webViewState = rememberWebViewState(url)
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            val loadedUrl = webViewState.lastLoadedUrl
-            if (loadedUrl != null && loadedUrl != url) {
-                Napier.v("user logged in: $loadedUrl")
-                val cookieManager = webViewState.cookieManager
-                CoroutineScope(Job() + Dispatchers.Default).launch {
-                    Napier.v("loaded url: ${cookieManager.getCookies(loadedUrl)}")
-                }
-            } else {
-                Napier.v("loaded url: $loadedUrl")
-                WebView(state = webViewState, modifier = Modifier.fillMaxSize())
-            }
-            val state = webViewState.loadingState
-            if (state is LoadingState.Loading) {
-                Napier.v("showing progress indicator")
-                LinearProgressIndicator(
-                    progress = state.progress,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            } else {
-                Napier.v("hiding progress indicator")
-            }
+    AppTheme {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "This should work!")
         }
     }
 }
+
+
+//Napier.base(DebugAntilog())
+//val url = "https://partner.swiggy.com/login"
+//var greetingText by remember { mutableStateOf("Hello, World!") }
+//var showImage by remember { mutableStateOf(false) }
+//val webViewState = rememberWebViewState(url)
+//Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//    val loadedUrl = webViewState.lastLoadedUrl
+//    if (loadedUrl != null && loadedUrl != url) {
+//        Napier.v("user logged in: $loadedUrl")
+//        val cookieManager = webViewState.cookieManager
+//        CoroutineScope(Job() + Dispatchers.Default).launch {
+//            Napier.v("loaded url: ${cookieManager.getCookies(loadedUrl)}")
+//        }
+//    } else {
+//        Napier.v("loaded url: $loadedUrl")
+//        WebView(state = webViewState, modifier = Modifier.fillMaxSize())
+//    }
+//    val state = webViewState.loadingState
+//    if (state is LoadingState.Loading) {
+//        Napier.v("showing progress indicator")
+//        LinearProgressIndicator(
+//            progress = state.progress,
+//            modifier = Modifier.fillMaxWidth()
+//        )
+//    } else {
+//        Napier.v("hiding progress indicator")
+//    }
+//}
 
